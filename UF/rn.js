69bf1e37a359560c4aa2934c4db76466b4a339be
pedro <devpeu.contact@gmail.com>
2023-08-05 20:52:29 -0300

    documentação

--- UF/rn.js
+++ UF/rn.js
@@ -11,20 +11,30 @@ async function getHtml() {
 
 getHtml().then((res) => {
    const $ = cheerio.load(res);
+
+   // Array de todas as informacoes menos o nome
    const arrayP = [];
+
+   // Array só de nomes de leiloeiros
    const arrayS = []
+
    const leiloeiros = [];
 
+   // Aqui é porque os dados desse antonio são especiais, o maluco que fez o site 
+   // colocou só ele diferentao e acabou quebrando todo o codigo mais tarde porque eu nao havia percebido
    const antonioText = $('div:nth-child(96)').text().trim();
 
    $('#P000 p').each((i, p) => {
+      // text recebe o text do paragrafo
       const text = $(p).text().trim();
 
+      // Obs: imaginando que a div #P000 é um array e os itens são paragrafos, o paragrafo 20 é o primeiro que contém info de um leiloeiro
       if(i > 19 && text.length > 1){
          arrayP.push(text)
       }
    })
 
+   // aqui eu apenas peguei os nomes dos leiloeiros que tem em comum a tag strong e mandei todos pro array designado
    $('strong').each((i, s) => {
       const text = $(s).text().trim();
 
@@ -33,11 +43,13 @@ getHtml().then((res) => {
       }
    })
 
+   // Esse array contém todos os dados de todos os leiloeiros mas sem os nomes
    const array = arrayP.filter((element) => !arrayS.includes(element));
 
    const fragmentos = [];
    let string = '';
 
+   // a contante em todos os dados é a nomeação, todo leiloeiro aqui tem uma. Portanto, eu percorri o array que só possuía os dados e estava mandando pro array 'fragmentos' todos os valores que começavam com 'Nomea' e os que não começavam eu concatenava no último item e mandava pro 'fragmentos' quando o próximo item começasse com 'Nomea'
    array.forEach((item, index) => {
       if (item.slice(0, 5) === 'Nomea' && (index === array.length - 1 || array[index+1].slice(0, 5) === 'Nomea')) {
          fragmentos.push(item);
@@ -50,9 +62,13 @@ getHtml().then((res) => {
       }
    });
 
+   // Essa é a função que vai retornar o objeto formatado
    function format(frags){
+
+      // Aqui eu encontrei um jeito de inserir o antonio no meio dessa galera sem quebrar a ordem, pois bagunçava todos os dados
       const array = [];
       let count = 1;
+
       arrayS.forEach((leiloeiro, i) => {
          if(leiloeiro === 'Antônio Romero Ferreira da Silva'){
             array.push(`nome: ${leiloeiro}\n${antonioText}`);
@@ -66,20 +82,36 @@ getHtml().then((res) => {
          }
       })
 
+
+      // daqui pra baixo é só manipulando os dados, geramente criando arrays de arrays, etc 
       let pessoas = [];
       array.forEach((leilo, i) => {
          pessoas.push([...leilo.split('\n')]);
       })
 
-      pessoas = pessoas.map((pessoa) => {
-         console.log(pessoa[0])
+      pessoas = pessoas.map((pessoa, i) => {
+         if (pessoa != '') {
+            return pessoa.map((prop) => {
+               return e = prop.split(': ');
+            });
+         } 
       });
 
+      const pessoasClone = [...pessoas]; 
+      pessoas = [];
+
+      pessoasClone.forEach((pessoa) => {
+         if(pessoa[pessoa.length -1][0]){
+             pessoas.push(pessoa)
+         }
+      })
+
+      return pessoas
    }
 
    format(fragmentos)
 
-   fs.writeFile('../data.json', JSON.stringify({}, null, 2), {encoding: "utf-8"} ,(err) => {
+   fs.writeFile('../data.json', JSON.stringify(format(fragmentos), null, 2), {encoding: "utf-8"} ,(err) => {
       if(err) throw err;
       console.log('webScrape successful')
    })
